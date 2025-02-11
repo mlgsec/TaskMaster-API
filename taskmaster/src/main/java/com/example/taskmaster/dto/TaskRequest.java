3a5400021f2a593e760e2eb3f707d11ef9469643
mlgsec <sec.mateuslgomes@gmail.com>
2025-02-11 19:23:26 -0300

    feat: adding validation

---
+++ taskmaster/src/main/java/com/example/taskmaster/dto/TaskRequest.java
@@ -0,0 +1,25 @@
+package com.example.taskmaster.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+public record TaskRequest (
+
+        @NotBlank(message = "Title is mandatory")
+        @Size(max = 100, message = "Title must be less than 100 characters")
+        String title,
+
+        @NotBlank(message = "Description is mandatory")
+        String description,
+
+        @NotNull(message = "Due date is mandatory")
+        LocalDate dueDate,
+
+        boolean completed
+
+        ) {
+}
